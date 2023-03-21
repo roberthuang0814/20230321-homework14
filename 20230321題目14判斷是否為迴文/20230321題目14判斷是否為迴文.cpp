97439956193d71297f1b5f888af3c79fe7439cac
rober <rober@LAPTOP-9BJTS009>
2023-03-21 23:45:02 +0800

    加入專案檔案。

---
+++ 20230321題目14判斷是否為迴文/20230321題目14判斷是否為迴文.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>  
+#include<string>  
+using namespace std;
+
+int main()
+{
+    string input, output;
+    bool a = 1;
+
+    cin >> input;
+    for (int i = input.length() - 1; i >= 0; i--)
+    {
+        output[input.length() - 1 - i] = input[i];
+    }
+    for (int j = 0; j < input.length(); j++)
+    {
+        if (input[j] != output[j]) { a = 0; break; }
+        else a = 1;
+    }
+    a ? cout << "YES" << endl : cout << "NO" << endl;
+    return 0;
+}
